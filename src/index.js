40e06ad64b4f35de50be9c613d8e581b2ae68b2c
13bit <128221545+The13bit@users.noreply.github.com>
2024-04-18 10:41:01 +0000

    please work

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ const App = () => {
           <Route component={Info} exact path="/Ngo-Website/info" />
 
           <Route component={NotFound} path="**" />
-
+{/* test */}
           <Redirect to="**" />
         </Switch>
         <Footer />
