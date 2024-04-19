784d240607e85d5dc944a3f2a06beaaf7a929b1e
The13bit <anasfodkar5@gmail.com>
2024-04-19 13:59:22 +0530

    updated icons

--- src/views/sucess.js
+++ src/views/sucess.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { Helmet } from 'react-helmet'
+import NGOICON from './../components/Images/GCAHF.png'
 const sucess = () => {
   return (
     <>
+    <Helmet>
+        <title>GCAHF</title>
+        
+        <link rel="icon" href={NGOICON} />
+      </Helmet>
         <div className=' tw-flex tw-flex-col tw-h-96 tw-text-center'>
             <p>Thank You For Donating</p>
         </div>
